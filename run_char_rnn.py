5b7dc9eb5136e7c598331c4dfd362452aa7977f1
pukkapies <knwebster@me.com>
2017-07-21 16:03:54 +0100

    Update server path and saving frequency

--- run_char_rnn.py
+++ run_char_rnn.py
@@ -3,23 +3,28 @@ import json
 from random import shuffle
 import shutil
 import numpy as np
-from utils.data_utils import load_data
+import time
+from utils.data_utils import load_data, check_data_for_quote, load_data_txt
 from utils.setup import make_settings
+from utils.vectorisation_utils import process_title_string
 from char_rnn import CharRNN
 
 
 
 # For server
-MODEL_FOLDER = '/home/kevin/pukkapies_github/SongTitles/saved_models/E128_L128_128/'
-TRAINING_DATA_PATH = "/home/kevin/pukkapies_github/SongTitles/data/first/training/"
-VALIDATION_DATA_PATH = "/home/kevin/pukkapies_github/SongTitles/data/first/validation/"
+MODEL_FOLDER = '/home/kevin/pukkapies_github/SongTitles/saved_models/E256_L256_256_128_second/'
+TRAINING_DATA_PATH = "/home/kevin/pukkapies_github/SongTitles/data/second/training/"
+VALIDATION_DATA_PATH = "/home/kevin/pukkapies_github/SongTitles/data/second/validation/"
 
 # For laptop
-# MODEL_FOLDER = '/Users/kevinwebster/tensorflow/songtitles/saved_models/E128_L128_128_len_average/'
-# TRAINING_DATA_PATH = "/Users/kevinwebster/tensorflow/songtitles/data/test_length/training/"
-# VALIDATION_DATA_PATH = "/Users/kevinwebster/tensorflow/songtitles/data/test_length/validation/"
+# MODEL_FOLDER = '/Users/kevinwebster/tensorflow/songtitles/saved_models/E256_L256_256_second/'
+# DATA_PATH = "/Users/kevinwebster/tensorflow/songtitles/data/second/"
+# TRAINING_DATA_PATH = DATA_PATH + "training/"
+# VALIDATION_DATA_PATH = DATA_PATH + "validation/"
 
-META_GRAPH = None  # "model-1750"
+META_GRAPH = None  # "model-8714"
+
+GENERATIONS_FILE = '/Users/kevinwebster/tensorflow/songtitles/generations_demo.txt'
 
 
 def main(MODEL_FOLDER=MODEL_FOLDER, TRAINING_DATA_PATH=TRAINING_DATA_PATH, VALIDATION_DATA_PATH=VALIDATION_DATA_PATH):
@@ -32,6 +37,8 @@ def main(MODEL_FOLDER=MODEL_FOLDER, TRAINING_DATA_PATH=TRAINING_DATA_PATH, VALID
         with open(MODEL_FOLDER + 'settings.json') as json_file:
             settings = json.load(json_file)
 
+        loaded_data_list = load_data_txt(DATA_PATH)
+
         # Load training and (optional) validation data
         print('\nloading training and validation data...')
         # TODO: The data structure isn't set up for this type of network. We will only use the 'output' data
@@ -43,20 +50,36 @@ def main(MODEL_FOLDER=MODEL_FOLDER, TRAINING_DATA_PATH=TRAINING_DATA_PATH, VALID
             model = CharRNN(settings, graph, MODEL_FOLDER, meta_graph=META_GRAPH)
             print('... done.')
 
-            generations = []
-            for _ in range(1000):
+
+            for _ in range(35):
                 generation = model.sample()
-                print(generation)
-                generations.append(generation)
-            long_count = 0
-            short_count = 0
-            for generation in generations:
-                if generation[:4] == 'good': long_count += 1
-                elif generation[:3] == 'get': short_count += 1
-                else: print('Generation not recognised: {}'.format(generation))
-            print("AVERAGING RESULTS: ")
-            print("Long count: ", long_count)
-            print("Short count: ", short_count)
+                if check_data_for_quote(generation, loaded_data_list):
+                    # print("QUOTE FOUND: ")
+                    continue
+                # print(generation)
+                generation = process_title_string(generation)
+                for char in generation:
+                    with open(GENERATIONS_FILE, 'a') as demo_file:
+                        demo_file.write(char)
+                        time.sleep(0.05)
+                with open(GENERATIONS_FILE, 'a') as demo_file:
+                    demo_file.write('\n')
+                    time.sleep(1.0)
+
+            # generations = []
+            # for _ in range(1000):
+            #     generation = model.sample()
+            #     print(generation)
+            #     generations.append(generation)
+            # long_count = 0
+            # short_count = 0
+            # for generation in generations:
+            #     if generation[:4] == 'good': long_count += 1
+            #     elif generation[:3] == 'get': short_count += 1
+            #     else: print('Generation not recognised: {}'.format(generation))
+            # print("AVERAGING RESULTS: ")
+            # print("Long count: ", long_count)
+            # print("Short count: ", short_count)
 
     else:  # Build a new model
         print("\nBuilding new model...")
